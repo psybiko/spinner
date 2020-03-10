e7e4c9843c42f537a4046aa385eae6067c9237d5
Kc Kai <kirstenckai@gmail.com>
2020-03-10 18:11:11 +0000

    Update refactored code. Worked with @ej2brown @hammadns96

--- spinner2.js
+++ spinner2.js
@@ -1,40 +1,13 @@
-const spinner = () => {
-  setTimeout(() => {
-    process.stdout.write('\r|  ');
-  }, 100);
-  
-  setTimeout(() => {
-    process.stdout.write('\r/  ');
-  }, 300);
-  
-  setTimeout(() => {
-    process.stdout.write('\r-  ');
-  }, 500);
-  
-  setTimeout(() => {
-    // Need to escape the backslash since it's a special character.
-    process.stdout.write('\r\\  ');
-  }, 700);
-  
-  setTimeout(() => {
-    process.stdout.write('\r|  ');
-  }, 900);
-  
-  setTimeout(() => {
-    process.stdout.write('\r/  ');
-  }, 1100);
-  
-  setTimeout(() => {
-    process.stdout.write('\r-  ');
-  }, 1300);
-  
-  setTimeout(() => {
-    process.stdout.write('\r\\  ');
-  }, 1500);
-  
-  setTimeout(() => {
-    process.stdout.write('\r|  ' + '\n');
-  }, 1700);
-};
+const spinner = ['\r|  ', '\r/  ', '\r-  ', '\r\\  ', '\r|  ', '\r/  ', '\r-  ', '\r\\  '];
 
-spinner();
+const delay = 200;
+let delaySum = 100;
+
+for (const line of spinner) {
+  setTimeout(() => {
+    process.stdout.write(line);
+  }, delaySum)
+  delaySum += delay;
+};
+ 
+
